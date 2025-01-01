ac9dfd242f6c983adb3693fd719a90d007d05126
Sk <shakil@meghops.com>
2025-01-01 23:05:31 +0600

    first test

---
+++ bin/newtestproject.dart
@@ -0,0 +1,9 @@
+main() {
+  var exampleTriangle = triangle(20, 20);
+  print(exampleTriangle);
+}
+
+double triangle( double base,double height) {
+  double area = 0.5 * base * height;
+
+  return area;
